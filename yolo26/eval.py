615983328b6a0cba2fd2d6cdba77777368cdaae3
Minjea31 <kimminjea31@naver.com>
2026-06-28 02:35:34 +0900

    edit eval logic

--- yolo26/eval.py
+++ yolo26/eval.py
@@ -1,16 +1,23 @@
-import sys
 from ultralytics import YOLO
+from compress.Compress import PruneHandler as ph
 import argparse
 
-if __name__=="__main__":
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_pt', type=str, default='../test_model.pt')
+parser.add_argument('--model_yaml', type=str, default=None)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default='../test_model.pt')
-    parser.add_argument('--data', type=str, default="../dataset.yaml")
+# parser.add_argument('--model_pt', type=str, default='/home/a/capstone/model/g040_i048.pt')
+# parser.add_argument('--model_yaml', type=str, default='/home/a/capstone/model/g040_i048.yaml')
 
-    args = parser.parse_args()
+parser.add_argument('--data', type=str, default="../dataset.yaml")
+parser.add_argument('--split', type=str, default="test", choices=["train", "val", "test"])
+parser.add_argument('--imgsz', type=int, default=640)
+parser.add_argument('--batch', type=int, default=2)
+parser.add_argument('--workers', type=int, default=4)
 
-    model = YOLO(args.model)
-
-    metrics = model.val(data=args.data)
-    print('mAP50:', metrics.box.map50)
+args = parser.parse_args()
+if args.model_yaml:
+    model = YOLO(args.model_yaml).load(args.model_pt)
+else:
+    model = YOLO(args.model_pt)
+model.val(data=args.data, split=args.split, imgsz=args.imgsz, batch=args.batch, workers=args.workers)
